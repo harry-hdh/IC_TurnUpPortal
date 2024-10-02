cd6d244062a284da3b7a14deb19b476cb196ebc8
harry-hdh <hdh997@outlook.com>
2024-10-02 23:31:14 +1300

    Optimize Wait method

--- IC_TurnUpWeb/Utilities/Wait.cs
+++ IC_TurnUpWeb/Utilities/Wait.cs
@@ -21,7 +21,22 @@ namespace IC_TurnUpWeb.Utilities
             }
             if (locatorType == "Id")
             {
-                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath(locatorValue)));
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id(locatorValue)));
+
+            }
+        }
+
+        public static void WaitToBeVisible(IWebDriver driver, string locatorType, string locatorValue, int seconds)
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, seconds));
+            if (locatorType == "Xpath")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(locatorValue)));
+
+            }
+            if (locatorType == "Id")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id(locatorValue)));
 
             }
         }
